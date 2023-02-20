d7635010af7a42bc7102d236b28d956e8bf11fb7
raybarkerrr <raybarkerrr@gmail.com>
2023-02-20 17:34:39 -0600

    Function fix
    Function wouldn't run because I forgot to assign an ID in HTML

--- server/public/javascript.js
+++ server/public/javascript.js
@@ -2,3 +2,8 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
+const button = document.querySelector("#checkout-button")
+
+button.addEventListener("click", () => {
+    console.log("Checkout")
+})
--- server/server.js
+++ server/server.js
@@ -2,16 +2,17 @@ require("dotenv").config()
 
 const express = require("express")
 const app = express()
-
+const cors = require("cors")
 app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5500",
+  })
+)
 
-const stripe = require("stripe")(process.env. STRIPE_PRIVATE_KEY)
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
 const storeItems = new Map([
-    [1, {priceInCents: 10000, name: "Price One"}],
-    [2, {priceInCents: 10000, name: "Price One"}] 
+  [1, { priceInCents: 10000, name: "Learn React Today" }],
+  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
 ])
-
-
-app.use(express.static("public"))
-app.listen(3000)
