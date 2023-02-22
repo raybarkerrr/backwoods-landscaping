8a439e1d63dc60480d456643182d35da3b39bc0b
raybarkerrr <raybarkerrr@gmail.com>
2023-02-21 18:41:10 -0600

    j

--- server/public/javascript.js
+++ server/public/javascript.js
@@ -2,8 +2,3 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
-const button = document.querySelector("#checkout-button")
-
-button.addEventListener("click", () => {
-    console.log("Checkout")
-})
