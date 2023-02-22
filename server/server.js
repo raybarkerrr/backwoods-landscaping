6a62fedbf348f0e47fb6996d0030f728639c1809
raybarkerrr <raybarkerrr@gmail.com>
2023-02-21 18:28:20 -0600

    Useless commit

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
-require("dotenv").config()
 
-const express = require("express")
-const app = express()
-const cors = require("cors")
-app.use(express.json())
-app.use(
-  cors({
-    origin: "http://localhost:5500",
-  })
-)
+// This is your test secret API key.
+const stripe = require('stripe')('sk_test_51Mdxn6HhyJdqB8YROOW39NcLw1AQdulnqw3lWcDPbFV7qw8T4uGidCgtETO6S3pZNMChADgy0qdyGkpuyNNrNLm500ASOigJ7l');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+const YOUR_DOMAIN = 'http://localhost:4242';
 
-const storeItems = new Map([
-  [1, { priceInCents: 10000, name: "Learn React Today" }],
-  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-])
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    automatic_tax: {enabled: true},
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
